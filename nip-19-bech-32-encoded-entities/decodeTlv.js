e9fe68da92ea678fce13a71cbdc71b823501a408
Asai Toshiya <to.asai.60@gmail.com>
2024-02-09 21:41:36 +0900

    変数の名前を変更する

--- nip-19-bech-32-encoded-entities/decodeTlv.js
+++ nip-19-bech-32-encoded-entities/decodeTlv.js
@@ -1,11 +1,11 @@
-const decodeTlv = (buffer, result = {}) => {
+const decodeTlv = (buffer, acc = {}) => {
   const t = buffer[0];
   const l = buffer[1];
   const v = buffer.slice(2, 2 + l);
-  const values = result[t] ?? [];
-  const newResult = { ...result, [t]: [...values, v] };
+  const values = acc[t] ?? [];
+  const result = { ...acc, [t]: [...values, v] };
   const rest = buffer.slice(2 + l);
-  return rest.length > 0 ? decodeTlv(rest, newResult) : newResult;
+  return rest.length > 0 ? decodeTlv(rest, result) : result;
 };
 
 module.exports = {
