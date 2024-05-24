65c58633c5fae4babd807ed7a231597e9635d1fc
Asai Toshiya <to.asai.60@gmail.com>
2024-05-25 01:15:40 +0900

    テストを追加する

--- nip-13-proof-of-work/nip13.test.js
+++ nip-13-proof-of-work/nip13.test.js
@@ -27,3 +27,19 @@ test("mine PoW and sign an event", async () => {
   expect(verifyEvent(signedEvent)).toBeTruthy();
   expect(nip13.getPow(signedEvent.id)).toEqual(21);
 });
+
+test("verify the example event", async () => {
+  const event = {
+    "id": "000006d8c378af1779d2feebc7603a125d99eca0ccf1085959b307f64e5dd358",
+    "pubkey": "a48380f4cfcc1ad5378294fcac36439770f9c878dd880ffa94bb74ea54a6f243",
+    "created_at": 1651794653,
+    "kind": 1,
+    "tags": [
+      ["nonce", "776797", "20"]
+    ],
+    "content": "It's just me mining my own business",
+    "sig": "284622fc0a3f4f1303455d5175f7ba962a3300d136085b9566801bc2e0699de0c7e31e44c81fb40ad9049173742e904713c3594a1da0fc5d2382a25c11aba977"
+  };
+
+  expect(verifyEvent(event)).toBeTruthy();
+});
